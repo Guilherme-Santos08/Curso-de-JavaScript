8c12b336e4e2b88fa70be571540e519f599dace9
Guilherme Augusto <augusto.gui.2208@gmail.com>
2020-10-23 13:42:49 -0200

    .

---
+++ aula12/ex012.js
@@ -0,0 +1,14 @@
+var hora = 0
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora >=06  && hora <=12) {
+  console.log('Bom Dia!')
+
+}else if (hora <= 18) {
+  console.log('Boa Tarde!')
+
+}else if (hora <=23){
+  console.log ('Boa Noite')
+} else {
+  console.log('Boa madrugada')
+}
+  
