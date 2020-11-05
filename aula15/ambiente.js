834f89a4b005f73b93da1190915fbbb68d57654d
Guilherme Augusto <augusto.gui.2208@gmail.com>
2020-11-05 21:28:31 -0200

    update

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3]
+
+num.sort() // sort vai deixar tudo em ordem!. Se deixar o push depois do sort, ele não vai organizar o push
+num.push(1) // push vai adcionar um numero no final.
+console.log(num)
+console.log(`O veteor tem ${num.length} posições`) // length serve para mostrar quantos vetores existe.
+console.log(`O primeiro valor do vetor é ${num[0]}`) // mostra o primeiro vetor.
