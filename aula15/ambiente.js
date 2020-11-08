efcded42fddf03e00686b09da550fbfa328d15f4
Guilherme Augusto <augusto.gui.2208@gmail.com>
2020-11-08 17:06:28 -0200

    update

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,7 +1,18 @@
+// Arrays
+
 let num = [5, 8, 2, 9, 3]
 
-num.sort() // sort vai deixar tudo em ordem!. Se deixar o push depois do sort, ele não vai organizar o push
 num.push(1) // push vai adcionar um numero no final.
+num.sort() // sort vai deixar tudo em ordem!. Se deixar o push depois do sort, ele não vai organizar o push
+
 console.log(num)
 console.log(`O veteor tem ${num.length} posições`) // length serve para mostrar quantos vetores existe.
 console.log(`O primeiro valor do vetor é ${num[0]}`) // mostra o primeiro vetor.
+
+let pos = num.indexOf(8)
+
+if(pos == -1) {
+  console.log('O valor não foi encontrado!')
+}else {
+  console.log(`O valor 8 está na posição ${pos}`)
+}
--- aula15/vetormartela.js
+++ aula15/vetormartela.js
@@ -1,5 +1,14 @@
-let num = [5, 8, 2, 9, 3]
+// Arrays 
 
-for(let pos = 0; pos<num.length ;pos++) {
-  console.log(pos)
+let valores = [5, 8, 2, 9, 3]
+
+
+//console.log(valores)
+
+/*for(let pos = 0; pos<valores.length ;pos++) {
+  console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for(let pos in valores) {  // Esse o jeito mais simplificado 
+  console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
